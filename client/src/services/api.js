73dfed06ff9407e962154ac3fc30569030c71460
Anupamadinesh <anupamadinesh31@gmail.com>
2026-03-17 19:36:51 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://wallet-backend.onrender.com/api"
+  baseURL: "https://wallet-backend-4rfm.onrender.com/api"
 });
 
 export const fetchDashboard = () => api.get("/dashboard");
